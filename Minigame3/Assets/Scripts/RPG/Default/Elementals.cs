6d708d075c229e9fc64dc9428ec6592a3430db1c
unknown <neronhat2907@gmail.com>
2023-11-05 21:10:04 +0700

    add logic element

--- Minigame3/Assets/Scripts/RPG/Default/Elementals.cs
+++ Minigame3/Assets/Scripts/RPG/Default/Elementals.cs
@@ -1,1 +1,69 @@
-﻿public enum element {Fire,ice,Lighting,Dark}
+﻿public enum element {None,Fire,ice,Lighting,Dark}
+
+namespace elementsystem
+{
+    public class Element
+    {
+        public static float logicelement(element ele,element ele2)
+        {
+            string result;
+            float ratioDmg=0;
+           switch (ele)
+            {
+                case element.None: { result = "";ratioDmg=1;break; }
+                case element.Fire: {
+                        switch(ele2)
+                        {
+                            case element.ice: { result = "weak";ratioDmg=0.7f; break;}
+                            case element.Lighting: { result = ""; ratioDmg = 1f; break; }
+                            case element.Dark: { result = "strong"; ratioDmg = 1.3f; break; }
+                            case element.Fire: { result = ""; ratioDmg = 1f; break; }
+                            case element.None: { result = "strong"; ratioDmg = 1.3f; break; }
+                        }
+
+                        break; }
+                case element.ice:
+                    {
+                        switch (ele2)
+                        {
+                            case element.ice: { result = ""; ratioDmg = 1f; break; }
+                            case element.Lighting: { result = "weak"; ratioDmg = 0.7f; break; }
+                            case element.Dark: { result = ""; ratioDmg = 1f; break; }
+                            case element.Fire: { result = "strong"; ratioDmg = 1.3f; break; }
+                            case element.None: { result = "strong"; ratioDmg = 1.3f; break; }
+                        }
+
+                        break;
+                    }
+                case element.Lighting:
+                    {
+                        switch (ele2)
+                        {
+                            case element.ice: { result = "strong"; ratioDmg = 1.3f; break; }
+                            case element.Lighting: { result = ""; ratioDmg = 1f; break; }
+                            case element.Dark: { result = "weak"; ratioDmg = 0.7f; break; }
+                            case element.Fire: { result = ""; ratioDmg = 1f; break; }
+                            case element.None: { result = "strong"; ratioDmg = 1.3f; break; }
+                        }
+
+                        break;
+                    }
+                case element.Dark:
+                    {
+                        switch (ele2)
+                        {
+                            case element.ice: { result = ""; ratioDmg = 1f; break; }
+                            case element.Lighting: { result = "strong"; ratioDmg = 1.3f; break; }
+                            case element.Dark: { result = ""; ratioDmg = 1f; break; }
+                            case element.Fire: { result = "weak"; ratioDmg = 0.7f; break; }
+                            case element.None: { result = "strong"; ratioDmg = 1.3f; break; }
+                        }
+
+                        break;
+                    }
+            }
+            return ratioDmg;
+        }
+            
+    }
+}
--- Minigame3/Assets/Scripts/RPG/Enemy/RPGenemyparrent.cs
+++ Minigame3/Assets/Scripts/RPG/Enemy/RPGenemyparrent.cs
@@ -1,22 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using elementsystem;
 
 public class RPGenemyparrent : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int HPmax;
-    public int Dmg;
+    public float HPmax;
+    public float Dmg;
    public element ResElement;
    public element MainElement;
     void Start()
     {
-        
+        //calele();
     }
+
+    public void calele()
+    {
+      Dmg=elementsystem.Element.logicelement(MainElement,ResElement);
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        calele();
     }
 }
